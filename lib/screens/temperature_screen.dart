f58725eee990da5016735551b8b6e6c310ec31dd
Marc AYENON <marc.ayenon@inexa-ci.com>
2023-09-07 12:56:29 +0000

    feat(feature-temp-screen): implémentation écran temperature

--- lib/screens/temperature_screen.dart
+++ lib/screens/temperature_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:neumorphism_flutter/charts/default_spline_chart.dart';
 import 'package:neumorphism_flutter/components/title_widget.dart';
 import 'package:neumorphism_flutter/constants.dart';
+import 'package:neumorphism_flutter/neumorphic/presets.dart';
 import 'package:neumorphism_flutter/neumorphic/thumbnail.dart';
 
 const double kThumbnailSize = 120;
@@ -15,6 +17,16 @@ class TemperatureScreen extends StatefulWidget {
 }
 
 class _TemperatureScreenState extends State<TemperatureScreen> {
+  late List<String> periods;
+  late String selectedPeriod;
+
+  @override
+  void initState() {
+    super.initState();
+    periods = ["Today", "Week", "Month"];
+    selectedPeriod = periods[0];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +38,7 @@ class _TemperatureScreenState extends State<TemperatureScreen> {
             slivers: [
               const SliverToBoxAdapter(
                 child: Padding(
-                  padding: EdgeInsets.only(left: 10.0),
+                  padding: EdgeInsets.only(left: 20.0),
                   child: TitleWidget(
                     littleTitle: "Good Morning,",
                     bigTitle: "Marc-Arnaud",
@@ -59,11 +71,91 @@ class _TemperatureScreenState extends State<TemperatureScreen> {
                     ),
                   ),
                 ),
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 20),
+                  child: Row(children: [
+                    Text(
+                      "Temperature",
+                      style: GoogleFonts.notoSans(
+                        color: kDarkBlueColor,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      " analytics",
+                      style: GoogleFonts.notoSans(
+                        color: kGrayedBlueColor,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    )
+                  ]),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 15),
+                  child: neumorphicNormalContainer(
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        children: [
+                          _buildPeriodRow(),
+                          const SplineDefault(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               )
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildPeriodRow() {
+    return Row(
+      children: periods
+          .map((period) => GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedPeriod = period;
+                  });
+                },
+                child: Padding(
+                    padding: const EdgeInsets.only(right: 10, bottom: 15),
+                    child: selectedPeriod == period
+                        ? neumorphicPressedContainer(Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10.0, vertical: 5),
+                            child: Text(
+                              period,
+                              style: GoogleFonts.notoSans(
+                                color: kDarkBlueColor,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ))
+                        : neumorphicNormalContainer(
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 10.0, vertical: 5),
+                              child: Text(period,
+                                  style: GoogleFonts.notoSans(
+                                    color: kGrayedBlueColor,
+                                    fontWeight: FontWeight.w600,
+                                  )),
+                            ),
+                          )),
+              ))
+          .toList(),
     );
   }
 
